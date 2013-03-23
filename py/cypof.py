f35dcd3add65401c8d6b3f37671f001dc7dcd111
cypof <cypof@0xdata.com>
2013-03-23 06:19:28 -0700

    Fixed debug launcher

--- py/cypof.py
+++ py/cypof.py
@@ -9,9 +9,9 @@ print 'cypof'
 #h2o.build_cloud_in_process()
 #h2o.build_cloud(node_count=3)
 
-#h2o.config_json = "pytest_config-cypof.json"
+h2o.config_json = "pytest_config-cypof.json"
 #h2o.config_json = "pytest_config-150-155.json"
-#h2o_hosts.build_cloud_with_hosts()
+h2o_hosts.build_cloud_with_hosts()
 
 h2o_cmd.runKMeans(csvPathname='smalldata/covtype/covtype.20k.data', key='covtype', k=7)
 n = h2o.nodes[0]
