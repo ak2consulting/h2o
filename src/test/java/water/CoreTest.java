33330d2696705b485a3ff6f65bd1201319ebfabb
Cliff Click <cliffc@acm.org>
2013-04-22 08:49:36 -0700

    Make all CoreTest keys go remote
    Bug is that the remote JVM loses a core to the DRemoteTask.
    Launch test with 2 JVMs, and the remote only uses 3 of 4 cores.

--- src/test/java/water/CoreTest.java
+++ src/test/java/water/CoreTest.java
@@ -15,11 +15,16 @@ public class CoreTest extends TestUtil {
   static int MAPTIME=1000;
 
   @Test public void testCPULoad() {
-    long start = System.currentTimeMillis();
     int jobs = H2O.NUMCPUS*RUNTIME/MAPTIME;
+    // Target all keys remotely: the bug is that the *remote* JVM loses a core
+    // to the DRemoteTask.
+    H2O cloud = H2O.CLOUD;
+    H2ONode target = cloud._memary[0];
+    if( target == H2O.SELF ) target = cloud._memary[1];
     Key[] keys = new Key[jobs];
     for( int i=0; i<keys.length; i++ )
-      keys[i] = Key.make("CPU"+i);
+      keys[i] = Key.make("CPU"+i,(byte)1,Key.DFJ_INTERNAL_USER,target);
+    long start = System.currentTimeMillis();
     new CPULoad().invoke(keys);
     long now=System.currentTimeMillis();
     Log.unwrap(System.err,"Runtime= "+(now-start)+" Jobs="+jobs+" maptime="+MAPTIME);
